788866932b474e04c9f4842236ea4b93016d0d1d
Alin Galben <alin98g@gmail.com>
2021-06-04 19:10:41 +0200

    Relaciones

---
+++ backend/src/models/relaciones.js
@@ -0,0 +1,7 @@
+import User from './User'
+import Producto from './Avatar';
+import Enlace from './Enlace';
+import Entrada from './Entrada';
+import Producto from './Producto';
+import Tienda from './Tienda';
+
