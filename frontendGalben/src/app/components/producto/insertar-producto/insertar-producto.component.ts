7d5241f424e81d0a64aa84cb65c138b5b438136e
Alin Galben <alin98g@gmail.com>
2021-06-11 03:37:46 +0200

    Muchos Cambios

--- frontendGalben/src/app/components/producto/insertar-producto/insertar-producto.component.ts
+++ frontendGalben/src/app/components/producto/insertar-producto/insertar-producto.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Producto } from 'src/app/models/producto/producto.model';
+import { ProductoService } from '../../../services/producto/producto.service';
+
 
 @Component({
   selector: 'app-insertar-producto',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InsertarProductoComponent implements OnInit {
 
-  constructor() { }
+ productos?: Producto[];
+
+  constructor(private productoService: ProductoService, private router:Router) { }
 
   ngOnInit(): void {
   }
+
+  permisos(): void{
+    this.productoService.getAllAdmin()
+    .subscribe(
+      data=> {
+        this.productos = data;
+      },
+      err => {
+        this.router.navigate(['/home'])
+      }
+    );
+  }
+
+  
+
+
 
 }
